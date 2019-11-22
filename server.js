f1dfa2ee6a8edc818e2f49fa3e0af9cb68a282f3
leach-chen <349763031@qq.com>
2019-11-22 19:12:48 +0800

    modify

--- server.js
+++ server.js
@@ -1,2 +1,2 @@
 const PeerServer = require('peer').PeerServer;
-const server = PeerServer({port: 9001, path: '/myapp'});
+const server = PeerServer({host:'192.168.1.122',port: 9001, path: '/myapp'});
