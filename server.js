e861382263884bf20e55d4b59ee9e3e0dbbf5bb5
leach-chen <349763031@qq.com>
2019-11-25 15:11:18 +0800

    添加websocket

--- server.js
+++ server.js
@@ -1,2 +1,15 @@
 const PeerServer = require('peer').PeerServer;
 const server = PeerServer({host:'192.168.1.145',port: 9001, path: '/myapp'});
+
+// server.js
+let WebSocket = require('ws');
+let instance = new WebSocket.Server({
+    host: '192.168.1.145',
+    port: 3000
+})
+instance.on('connection', client => {
+    client.on('message', function (message) {
+        client.send('收到！');
+        console.log(message)
+    })
+})
