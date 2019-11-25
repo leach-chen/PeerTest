19011eb1320c40128ceb949e87cf3bdb386028a4
leach-chen <349763031@qq.com>
2019-11-25 10:18:10 +0800

    modify ip

--- server.js
+++ server.js
@@ -1,2 +1,2 @@
 const PeerServer = require('peer').PeerServer;
-const server = PeerServer({host:'192.168.1.122',port: 9001, path: '/myapp'});
+const server = PeerServer({host:'192.168.1.145',port: 9001, path: '/myapp'});
