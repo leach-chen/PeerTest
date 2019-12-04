527ece22fbc08dd4e240bd4b0cd6382fd064963d
leach-chen <349763031@qq.com>
2019-12-04 15:50:04 +0800

    modify

--- js/test.js
+++ js/test.js
@@ -4,9 +4,7 @@ fs.readFile('../input.txt', function (err, data) {
     console.log(data.toString());
 });
 console.log("程序执行结束!");
-
-
-
+//----------------------------------------------
 var events = require("events")
 var eventEmitter = new events.EventEmitter()
 var eventHandler = function () {
@@ -14,3 +12,22 @@ var eventHandler = function () {
 }
 eventEmitter.on('aa', eventHandler);
 eventEmitter.emit('aa');
+
+//----------------------------------------------
+var http = require("http");
+var url = require("url");
+function onRequest(request, response) {
+    var pathname = url.parse(request.url).pathname;
+    console.log(pathname)
+    var arg = url.parse(request.url,true).query
+    console.log(arg)
+    if(arg != null) {
+        console.log(arg.aa)
+    }
+    console.log("--------------------")
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello World");
+    response.end();
+}
+http.createServer(onRequest).listen(8888);
